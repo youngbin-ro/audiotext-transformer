3b2f0d0f1df4e051a2b0af77c0c2151a81aa91d9
youngbin-ro <youngbin.ro@gmail.com>
2020-10-19 02:08:18 +0900

    revise: typo

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         datefmt="%m/%d/%Y %H:%M:%S",
-        level=logging.INFO,
+        level=logging.INFO
     )
 
     main(args_)
