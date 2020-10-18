c0bdfd359a198d56f231d29ef65f95a58e3309b8
youngbin-ro <youngbin.ro@gmail.com>
2020-10-19 01:52:17 +0900

    add: function for setting seed

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 import pickle
+import random
+import torch
+import numpy as np
 
 
 def load_pkl(path):
     with open(path, 'rb') as f:
         return pickle.load(f)
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
