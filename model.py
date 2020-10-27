78666507a224a270289fd4801fc594bc9eaba1ff
youngbin-ro <youngbin.ro@gmail.com>
2020-10-27 23:05:10 +0900

    add: function for loading pre-trained bert

---
+++ model.py
@@ -0,0 +1,11 @@
+import os
+import torch
+from transformers import BertConfig, BertModel
+
+
+def get_bert(bert_path, device):
+    bert_config_path = os.path.join(bert_path, 'bert_config.json')
+    bert = BertModel(BertConfig(bert_config_path)).to(device)
+    bert_model_path = os.path.join(bert_path, 'model.bin')
+    bert.load_state_dict(torch.load(bert_model_path))
+    return bert
