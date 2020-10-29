54b510cd6a6460b6d16a473d5b9a931256cb06d9
youngbin-ro <youngbin.ro@gmail.com>
2020-10-29 11:56:11 +0900

    add: function for getting optimizer and scheduler

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import random
 import torch
 import json
 import numpy as np
+import torch.optim as optim
+from warmup_scheduler import GradualWarmupScheduler
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 def set_seed(seed):
@@ -19,3 +22,16 @@ def load_pkl(path):
 def load_json(path):
     with open(path, 'r') as f:
         return json.load(f)
+
+
+def get_optimizer_and_scheduler(args, model):
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = ReduceLROnPlateau(optimizer, verbose=True)
+    if args.warmup_steps != 0:
+        scheduler = GradualWarmupScheduler(
+            optimizer=optimizer,
+            multiplier=1,
+            total_epoch=args.warmup_steps,
+            after_scheduler=scheduler
+        )
+    return optimizer, scheduler
